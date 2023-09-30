cbbaa508614c6e82445ff5e2e4e98768aca916eb
Anna_Paulini <anna.paulini@netlogix.de>
2023-09-30 19:01:37 +0200

    add function to determine accuracy for addition and subtraction

--- app/src/Service/CalcService.php
+++ app/src/Service/CalcService.php
@@ -4,14 +4,30 @@ namespace Netlogix\PhpUnit\Service;
 
 class CalcService
 {
+
+    private function detLenFractPart (float $a): int
+    {
+        $numberFractionPartLength = 0;
+        $a_string = strval($a);
+        $begin_fractionpart = strpos($a, ".");
+        if ($begin_fractionpart !== false) {
+            $numberFractionPartLength = strlen($a_string) - $begin_fractionpart - 1;
+        }
+        return $numberFractionPartLength;
+    }
+
     function addition(float $a, float $b): float
     {
-        return $a + $b;
+        $accuracy = max ($this->detLenFractPart($a), $this->detLenFractPart($b));
+        $result = $a + $b;
+        return round($result, $accuracy);
     }
 
     function subtraction(float $a, float $b): float
     {
-        return $a - $b;
+        $accuracy = max ($this->detLenFractPart($a), $this->detLenFractPart($b));
+        $result = $a - $b;
+        return round($result, $accuracy);
     }
 
     function multiplication(float $a, float $b): float
@@ -21,6 +37,7 @@ class CalcService
 
     function division(float $a, float $b): float
     {
+
         return $a / $b;
     }
 }
