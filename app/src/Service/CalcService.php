4886b20c41ce6f60bb76a0fdd554ceeba8379cc0
Anna_Paulini <anna.paulini@netlogix.de>
2023-10-12 10:23:13 +0200

    Deactivate unnecessary testcases

--- app/src/Service/CalcService.php
+++ app/src/Service/CalcService.php
@@ -7,13 +7,79 @@ class CalcService
 
     private function detLenFractPart (float $a): int
     {
-        $numberFractionPartLength = 0;
+//        $numberFractionPartLength = 0;
         $a_string = strval($a);
-        $begin_fractionpart = strpos($a, ".");
-        if ($begin_fractionpart !== false) {
-            $numberFractionPartLength = strlen($a_string) - $begin_fractionpart - 1;
+        $parts = explode(".", $a_string);
+         return strlen($parts[1]??"");
+//        $begin_fractionpart = strpos($a, ".");
+//        if ($begin_fractionpart !== false) {
+//            $numberFractionPartLength = strlen($a_string) - $begin_fractionpart - 1;
+//        }
+//        return $numberFractionPartLength;
+    }
+
+    private function detFuzzinessStart (float $a): int
+    {
+        $fuzzy_start = -1;
+        $a_string = strval($a);
+
+
+//        $a_string = strval(0.7499999999999999);
+//        echo $a_string . "\n";
+//        echo "0.7499999999999999" . "\n";
+//        echo 0.7499999999999999 . "\n";
+//        echo $a . "\n";
+
+
+        $parts = explode(".", $a_string);
+
+//        print_r($parts);
+//        $parts = explode(".", "0.7499999999999999");
+//        print_r($parts);
+//        $parts = explode(".", "0.20000000000000004");
+//        print_r($parts2);
+//        $parts = explode(".", "2.0100000000000002");
+//        print_r($parts);
+//        $parts = explode(".", "2.0100000300000002");
+//        print_r($parts4);
+
+
+        if ($parts[1] === null) {
+            echo $fuzzy_start . "\n";
+            return $fuzzy_start;
+//        } elseif ((str_contains($parts[1], "0") === false) && !(str_contains($parts[1], "9")=== false)) {
+////            $fuzzy_start = -2;
+////            echo strpos($parts[1], "0") . "\n";
+////            echo $fuzzy_start . "\n";
+//            return $fuzzy_start;
+        } else {
+            $pattern_9 = "/(^[0-8]+)(9{3,}$)/";
+            $pattern_0x = "/(^[0-9]*[1-9])(0{3,}[1-9]$)/";
+
+            $found_pattern = preg_match($pattern_9, $parts[1], $matches, PREG_OFFSET_CAPTURE);
+            if ($found_pattern === 0) {
+                $found_pattern = preg_match($pattern_0x, $parts[1], $matches, PREG_OFFSET_CAPTURE);
+//                print_r($matches);
+                if ($found_pattern === 0) {
+                    return $fuzzy_start;
+                }
+            }
+//            print_r($matches);
+            $fuzzy_start = $matches[2][1];
+            echo $fuzzy_start . "\n";
+
+//            preg_match($pattern_9, $parts0[1], $matches);
+//            print_r($matches);
+//            preg_match($pattern_0x, $parts2[1], $matches);
+//            print_r($matches);
+//            preg_match($pattern_0x, $parts3[1], $matches);
+//            print_r($matches);
+//            preg_match($pattern_0x, $parts4[1], $matches);
+//            print_r($matches);
+
+
+            return $fuzzy_start;
         }
-        return $numberFractionPartLength;
     }
 
     function addition(float $a, float $b): float
@@ -32,12 +98,24 @@ class CalcService
 
     function multiplication(float $a, float $b): float
     {
-        return $a * $b;
+        $accuracy = $this->detLenFractPart($a) + $this->detLenFractPart($b);
+        $result = $a * $b;
+        return round($result, $accuracy);
     }
 
     function division(float $a, float $b): float
     {
+        $result = $a / $b;
+//        $is_fuzzy = $this->detFuzzinessStart($a / $b);
+//        echo $is_fuzzy . "\n";
+//        if ($is_fuzzy === -1) {
+//            return $result;
+//        } else {
+//            return round($result, $is_fuzzy - 1);
+//        }
+//        //        echo $result . "\n";
+////        $this->detFuzzinessStart(0.7499999999999999);
 
-        return $a / $b;
+        return $result;
     }
 }
--- app/src/app.php
+++ app/src/app.php
@@ -7,3 +7,10 @@ $calc = new \Netlogix\PhpUnit\Service\CalcService();
 dump($calc->addition(186.864, 2));
 dump($calc->subtraction(1, 2));
 //dump($calc->division(1, 0));
+
+dump($calc->division(-0.6, 0.8));
+dump($calc->division(-0.14, -0.7));
+var_dump($calc->division(-0.6, 0.8));
+var_dump($calc->division(-0.14, -0.7));
+
+echo $calc->division(-0.6, 0.8);
